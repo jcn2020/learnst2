5748707732c895ba075d3ab9de3c4f4f31cff8bc
James Nguyen <jnguyen@starbucks.com>
2023-07-18 12:13:27 -0700

    update

--- actions/workflows/queryst2/main.py
+++ actions/workflows/queryst2/main.py
@@ -5,12 +5,16 @@ from st2common.runners.base_action import Action
 
         
 class QuerySt2(Action):
-    def run(self, directory_name):
-        print("directory name = " + directory_name)
-        mypacks = { 'name': os.path.basename(directory_name),
+    def run(self, pack_name):
+        print("directory name = " + pack_name)
+        
+        path = r"/opt/stackstorm/packs"
+        packFullPath = os.path.join(path, pack_name)
+        # check if pack available
+        mypacks = { 'name': os.path.basename(packFullPath),
                     'secretList': [] }
         p = re.compile(r"\S+.yaml")
-        for root, d_names, f_names in os.walk(directory_name):
+        for root, d_names, f_names in os.walk(packFullPath):
             for f in f_names:
                 if p.match(f):
                     fullpath = os.path.join(root,f)
