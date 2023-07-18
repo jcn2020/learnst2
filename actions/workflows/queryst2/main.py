23596d56e8493bd2fd322cc4ff5dcd066cfc9b81
James Nguyen <jnguyen@starbucks.com>
2023-07-17 19:43:32 -0700

    update

--- actions/workflows/queryst2/main.py
+++ actions/workflows/queryst2/main.py
@@ -3,5 +3,5 @@ import os, sys, json, re
 from st2common.runners.base_action import Action
 
 class QuerySt2(Action):
-    def run(self, directoryName):
-        print("directory name = " + directoryName)
+    def run(self, directory_name):
+        print("directory name = " + directory_name)
