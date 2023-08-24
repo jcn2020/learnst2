41222e0aac1c595b73046c3afa0bb003ef06a7dd
James Nguyen <jnguyen@starbucks.com>
2023-08-24 12:32:02 -0700

    update

--- actions/workflows/queryst2/main.py
+++ actions/workflows/queryst2/main.py
@@ -9,21 +9,22 @@ class QuerySt2(Action):
         print("directory name = " + pack_name)
         
         # looping through /opt/stackstorm/packs
-        packs = {}
+        pack_secrets = {}
         secret_list = []
         path = r"/opt/stackstorm/packs"
+
         ## find different packs in packs
         pack_list = [name for name in os.listdir(path)]
         print("========")
         print(pack_list)
-        print("========")
+        print("=== LIST PACKS /opt/stackstorm =====")
 
         for pack in pack_list: 
           print("=====> pack = ", pack)
           secret_list = []
-          packFullPath = os.path.join(path, pack)
+          pack_fullpath = os.path.join(path, pack)
 
-          # check if pack available
+          # check for all secrets in st2kv.system.* 
           p = re.compile(r"\S+.yaml")
           for root, d_names, f_names in os.walk(packFullPath):
               for f in f_names:
@@ -37,5 +38,8 @@ class QuerySt2(Action):
                         if m not in secret_list and m:
                             secret_list.append(m)
                     fh.close()
-          if secret_list: packs[pack] = secret_list
+          if secret_list: pack_secrets[pack] = secret_list
         print(packs)
+        # find the current working pwd
+        print("======== Current PWD ===== ")
+        print(os.listdir())
