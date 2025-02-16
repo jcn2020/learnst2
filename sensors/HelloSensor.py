9e10d239afceb7f349abb52e98283f1a3a452cef
James Nguyen <jnguyen@starbucks.com>
2025-02-16 03:20:16 -0800

    update

--- sensors/HelloSensor.py
+++ sensors/HelloSensor.py
@@ -1,10 +1,12 @@
 import eventlet
 
 # from st2reactor.sensor.base import Sensor
-from st2reactor.sensor.base import PollingSensor
+from st2common.runners.base_action import Action
+from st2common.persistence.trigger import TriggerType
+from st2reactor.sensor.base import Sensor
 
 
-class HelloSensor(PollingSensor):
+class HelloSensor(Sensor):
     def __init__(self, sensor_service, config):
         super(HelloSensor, self).__init__(sensor_service=sensor_service, config=config)
         self._logger = self.sensor_service.get_logger(name=self.__class__.__name__)
@@ -16,7 +18,7 @@ class HelloSensor(PollingSensor):
         pass
 
     # def run(self):
-    def poll(self):
+    def run(self):
         while not self._stop:
 
             print(f"value of api_key is {config['api_key']}" ) 
